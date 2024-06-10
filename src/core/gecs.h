0e113b745e4bee4bbf0b156200b46d5ee3ce5be9
EmilChoparinov <emilchoparinov@gmail.com>
2024-06-10 22:15:16 +0200

    setup finally ready

---
+++ src/core/gecs.h
@@ -0,0 +1,25 @@
+/* =========================================================================
+    Author: E.D Choparinov, Amsterdam
+    Related Files: gecs.h gecs.c
+    Created On: June 10 2024
+    Purpose:
+        Entrypoint to the GECS Framework. Include this to include the
+        library in your project.
+========================================================================= */
+
+#ifndef __HEADER_GECS_H__
+#define __HEADER_GECS_H__
+
+/*-------------------------------------------------------
+ * GECS Core
+ *-------------------------------------------------------*/
+#include "gecs_codes.h"    /* Contains return code information. */
+#include "gecs_register.h" /* Registers container types. */
+
+/*-------------------------------------------------------
+ * Utilities
+ *-------------------------------------------------------*/
+#include "debug.h"  /* Adds GECS debugging features. */
+#include "logger.h" /* Adds Logging */
+
+#endif
